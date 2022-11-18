e173e161f1bf72b94b14350953ccae4080a7f4a6
ryan anderson <ryanmanderson1986@gmail.com>
2022-11-18 13:10:58 -0600

    retry initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,4 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-
-//import node from "@astrojs/node";
 
 // https://astro.build/config
-export default defineConfig({
-  integrations: [tailwind()],
-  /* output: "server",
-  adapter: node({
-    mode: 'standalone'
-  }), */
-});
+export default defineConfig();
